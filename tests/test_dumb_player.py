c6d587c53e9371636ab8cc2ca6dfa98225acf9fc
Colby Roberts <colbiverse@gmail.com>
2020-07-17 22:30:40 +0000

    fixing logging and log tests pt.1

---
+++ tests/test_dumb_player.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('src')
+
+from logging import Logging
+
+log = Logging()
+example = Logging
+
+log.get_current_active_file('space-empires/logs')
+
+contents = log.read_info()
+
+correct_example.get_current_active_file('space-empires/test_logs', 'test_dumb_player')
+
+test = correct_example.read_info()
+
+with log.active_file as text, correct_example.active_file as exc: #print lines that are different from the correct example
+    exclusions = [line.rstrip('\n') for line in exc]
+    for line in text:
+        if not any(exclusion in line for exclusion in exclusions):
+            print line
+
+assert contents == test, "The dumb player isn't as dumb as you think"
+
+
+
+print('Dumb Player Works!!!')
